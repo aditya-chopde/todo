c6689484a42af69ae83e324e77ef29d780cc5b8d
Aditya Chopde <its.adityac@gmail.com>
2025-01-16 19:42:14 +0530

    added get single task route

---
+++ app/api/single/[id]/route.ts
@@ -0,0 +1,22 @@
+import { Task } from "@/lib/models/Task";
+
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const id = (await params).id;
+    const findSingleTask = await Task.findById(id);
+    return Response.json({
+      success: true,
+      message: "Task Fetched",
+      task: findSingleTask,
+    });
+  } catch (error) {
+    if(error instanceof Error){
+        return Response.json({success: false, message: "Error Occurred", error: error.message});
+    }else{
+        return Response.json({success: false, message: "Unknown Error Occurred"})
+    }
+  }
+}
