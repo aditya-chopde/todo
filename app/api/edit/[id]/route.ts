0e224e7b0d0d226af8266b01a0fc870a1b5ec280
Aditya Chopde <its.adityac@gmail.com>
2025-01-16 19:27:23 +0530

    added a edit task route in teh backend

---
+++ app/api/edit/[id]/route.ts
@@ -0,0 +1,29 @@
+import { Task } from "@/lib/models/Task";
+
+export async function POST(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const id = (await params).id;
+    const formData = await req.formData();
+    const title = formData.get("title");
+    const description = formData.get("description");
+    const updateTask = await Task.findByIdAndUpdate(id, {
+      title,
+      description,
+    });
+
+    return Response.json({
+      success: true,
+      message: "Task Edited",
+      task: updateTask,
+    });
+  } catch (error) {
+    if(error instanceof Error){
+        return Response.json({success: true, message: "Error Occurred", error: error.message});
+    }else{
+        return Response.json({success: false, message: "Unknown Error Occurred"})
+    }
+  }
+}
