a16f8dc40933a44a1039cbbdcfd35d47284826b5
Aditya Chopde <its.adityac@gmail.com>
2025-01-16 19:10:51 +0530

    implemented get all tasks in the banckend

--- app/api/get/route.ts
+++ app/api/get/route.ts
@@ -1,3 +1,14 @@
-export async function GET(req:Request) {
-    
+import { Task } from "@/lib/models/Task";
+
+export async function GET() {
+    try {
+        const getTasks = await Task.find();
+        return Response.json({success: true, message: "Task Fetched", tasks: getTasks})
+    } catch (error) {
+        if(error instanceof Error){
+            return Response.json({success: false, message: "Error Occurred", error: error.message})
+        }else{
+            return Response.json({success: false, message: "Unknown Error Occurred"})
+        }
+    }
 }
