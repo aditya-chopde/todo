b92c26a9c55b8ce6f8140d938fe52d50b56ab11f
Aditya Chopde <its.adityac@gmail.com>
2025-01-16 18:53:26 +0530

    created a create task route

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -1,10 +1,19 @@
+import { connectDB } from "@/lib/connectDb";
+import { Task } from "@/lib/models/task";
+
 export async function GET(req: Request){
     return Response.json({message: "Working"})
 }
 
 export async function POST(req: Request){
+    await connectDB();
     const formData = await req.formData();
     const title = formData.get("title")
     const description = formData.get("description")
-    return Response.json({title, description})
+    
+    const createTask = await Task.create({
+        title, description
+    })
+
+    return Response.json({success: true, message: "Task Created", task: createTask});
 }
