7f8e4312463568eb5b6f9cc408037cd2552e0b71
Aditya Chopde <its.adityac@gmail.com>
2025-01-17 21:28:24 +0530

    small changes

--- components/EnterTask.tsx
+++ components/EnterTask.tsx
@@ -1,9 +1,25 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import Task from "./Task";
+import axios from "axios";
 
 const EnterTask = () => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+
+  async function handleCreateTask(e: FormEvent){
+    e.preventDefault()
+    const formData: {title: string, description: string} = {
+      title, 
+      description,
+    }
+    await axios.post("./api/create", formData, {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    }).then((res)=>{
+      console.log(res.data)
+    });
+  }
 
   useEffect(() => {
     setIsLoaded(true);
@@ -14,13 +30,15 @@ const EnterTask = () => {
   }
   return (
     <>
-      <form className="max-w-md mx-auto my-10">
+      <form className="max-w-md mx-auto my-10" onSubmit={handleCreateTask}>
         <div className="relative z-0 w-full mb-5 group">
           <input
+          value={title}
+          onChange={(e)=> setTitle(e.target.value)}
             type="text"
             name="floating_email"
             id="floating_email"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
           />
@@ -33,10 +51,12 @@ const EnterTask = () => {
         </div>
         <div className="relative z-0 w-full mb-5 group">
           <input
-            type="password"
+          value={description}
+          onChange={(e)=> setDescription(e.target.value)}
+            type="text"
             name="floating_password"
             id="floating_password"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
           />
