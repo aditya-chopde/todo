fbc296b46bd1f6adbd26f4150e171b5b997732e8
Aditya Chopde <its.adityac@gmail.com>
2025-01-18 20:25:05 +0530

    implemented mark the taks as doen route in backend

---
+++ app/api/done/[id]/route.ts
@@ -0,0 +1,26 @@
+import { connectDB } from "@/lib/connectDb";
+import { Task } from "@/lib/models/Task";
+
+export async function POST(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  await connectDB()
+  try {
+    const id = (await params).id;
+    const updateTask = await Task.findByIdAndUpdate(id, {
+      status: true
+    });
+    return Response.json({
+      success: true,
+      message: "Task Mark As Done",
+      task: updateTask,
+    });
+  } catch (error) {
+    if(error instanceof Error){
+        return Response.json({success: true, message: "Error Occurred", error: error.message});
+    }else{
+        return Response.json({success: false, message: "Unknown Error Occurred"})
+    }
+  }
+}
